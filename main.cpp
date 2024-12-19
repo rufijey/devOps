47f9ad0c51358ed989de53405ed8328d9b84d809
sanya <sanya@ubuntu.myguest.virtualbox.org>
2024-12-19 20:58:22 +0200

    singal child and interapt

--- main.cpp
+++ main.cpp
@@ -1,10 +1,44 @@
 #include <iostream>
+#include <sys/wait.h>
 #include "ClassA.h"
 
 int CreateHTTPserver();
-int main() {
-	// ClassA classA;
-	// std::cout << "FuncA result: " << classA.FuncA(5, 7.0) << std::endl;
+
+void sigchldHandler(int s)
+{
+	printf("Caught signal SIGCHLD\n");
+
+	pid_t pid;
+	int status;
+	
+	while((pid = waitpid(-1,&status,WNOHANG)) > 0)
+	{
+		if(WIFEXITED(status)) printf("\nChild process termainated");
+	}
+
+}
+
+void sigintHandler(int s)
+{
+        printf("Caught signal %d. Starting graceful exit procedure\n",s);
+
+        pid_t pid;
+        int status;
+
+        while((pid = waitpid(-1,&status,WNOHANG)) > 0)
+        {
+                if(WIFEXITED(status)) printf("\nChild process termainated");
+        }
+
+	if (pid == -1) printf("\nAll child processes termainated");
+	
+	exit(EXIT_SUCCESS);
+
+}
+
+int main(int argc, char* argv[]) {
+	signal(SIGCHLD, sigchldHandler);
+	signal(SIGINT, sigintHandler);
 	CreateHTTPserver();
 	return 0;
 }
